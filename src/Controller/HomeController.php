21bbbd1971b2ba6a7aaec1850e052b54497dc9df
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2020-11-27 17:39:22 +0100

    home route on annotation + use AbstractController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,23 +1,20 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends abstractController
 {
 
     /**
-     * @param Environment $twig
-     */
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-
-    
+    * @Route("/", name="home")
+    *
+    * @return Response
+    */
     public function index(): Response
     {
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return new Response($this->render('pages/home.html.twig'));
     }
 }
