e7e068da7da68ee229f4e9ac95d7913e331d6e83
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2020-11-27 17:09:38 +0100

    first page in routes.yaml + Twig integration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,22 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class HomeController
 {
 
+    /**
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    
     public function index(): Response
     {
-        return new Response('Hello World!');
+        return new Response($this->twig->render('pages/home.html.twig'));
     }
 }
