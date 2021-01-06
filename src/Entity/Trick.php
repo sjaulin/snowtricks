15d6a3878d852024bf5dc5f36f718da5d052e50a
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2021-01-06 10:16:24 +0100

    New fields on trick entity

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -38,6 +38,16 @@ class Trick
      */
     private $category;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mainPicture;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->created_at = new \DateTime();
@@ -92,6 +102,30 @@ class Trick
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getMainPicture(): ?string
+    {
+        return $this->mainPicture;
+    }
+
+    public function setMainPicture(string $mainPicture): self
+    {
+        $this->mainPicture = $mainPicture;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
