c571d5a55a72268ca54cb269f1e66b44a4fac1c7
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2021-01-16 17:09:44 +0100

    Fixture User & init Authenticator

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
