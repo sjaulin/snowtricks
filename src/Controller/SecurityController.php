8e6fac9a13518bc86aed005b3755ef79dc98d7b9
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2021-01-18 10:21:31 +0100

    User Login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,18 +6,21 @@ use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="security_login")
+     * @return Response
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $utils): Response
     {
         $form = $this->createForm(LoginType::class);
-
+        $error = $utils->getLastAuthenticationError(); // Récupère les erreurs via attributs ou session
         return $this->render('security/login.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'error' => $error
         ]);
     }
 }
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,42 +3,72 @@
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
 
 class LoginFormAuthenticator extends AbstractGuardAuthenticator
 {
+
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    // Quels conditions l'authenticator contrôle la route.
     public function supports(Request $request)
     {
-        // todo
+        return $request->attributes->get('_route') === 'security_login'
+            && $request->isMethod('POST');
     }
 
+    // Retourne les informations nécessaires à l'authentification.
     public function getCredentials(Request $request)
     {
-        // todo
+        return $request->request->get('login'); // array avec 3 éléments
     }
 
+    // Vérifiee la présence de l'utilisateur via le provider
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        try {
+            return $userProvider->loadUserByUsername($credentials['email']);
+        } catch (UsernameNotFoundException $e) {
+            throw new AuthenticationException('Cet utilisateur n\'exite pas');
+        }
     }
 
+    // Vérifie que le mot de passe fournit est le même que celui du provider.
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        $isvalid = $this->encoder->isPasswordValid($user, $credentials['password']);
+        if (!$isvalid) {
+            throw new AuthenticationException('Mot de passe incorrect');
+        }
+
+        return $isvalid;
     }
 
+    // En cas d'erreur sur l'une des étapes précédentes
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        // todo
+        // On enregistre l'exception levé dans les attributs pour la retrouver dans le getLastAuthenticationError du securityController.
+        $request->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        // todo
+        // TODO message de success
+        return new RedirectResponse("/");
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
